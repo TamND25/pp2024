64e83482289cee9662e38f8f3dc92e4823e50462
YukiMakoto25 <tamnd.22bi13400@usth.edu.vn>
2024-01-17 10:39:04 +0700

    Creating Course class

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -13,3 +13,13 @@ class Student:
     def showdob(self):
         return self.__dob
         
+class Course:
+    def __init__(self, name, id):
+        self.__name = name
+        self.__id = id
+
+    def showname(self):
+        return self.__name
+    
+    def showid(self):
+        return self.__id
