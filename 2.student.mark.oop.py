2914fd17056da604d95d13d9ac9775a40d16c768
YukiMakoto25 <tamnd.22bi13400@usth.edu.vn>
2024-01-17 10:35:59 +0700

    Adding methods to Student class

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -1,5 +1,15 @@
 class Student:
     def __init__(self, name, id, dob):
-        self.name = name
-        self.id = id
-        self.dob = dob
+        self.__name = name
+        self.__id = id
+        self.__dob = dob
+    
+    def showname(self):
+        return self.__name
+    
+    def showid(self):
+        return self.__id
+    
+    def showdob(self):
+        return self.__dob
+        
