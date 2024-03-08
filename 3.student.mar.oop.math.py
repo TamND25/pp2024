e7e11f7ce5bb912f1827d7bf15460dd0284b8dd0
YukiMakoto25 <tamnd.22bi13400@usth.edu.vn>
2024-03-08 14:35:32 +0700

    Fix message in file 3

--- 3.student.mar.oop.math.py
+++ 3.student.mar.oop.math.py
@@ -73,7 +73,7 @@ class CoursesList:
     def show_courses_list(self, stdscr):
         stdscr.clear()
         for course in self.__courses:
-            stdscr.addstr(f"Name: {course.show_name()}, ID: {course.show_id()}, Crdeit: {course.show_credit()}\n")
+            stdscr.addstr(f"Name: {course.show_name()}, ID: {course.show_id()}, Credit: {course.show_credit()}\n")
         stdscr.addstr("Press any key to close!")
         stdscr.getch()    
         stdscr.clear()
